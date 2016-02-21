38ad0e27561be88398859919e5227a30b24243c4
Alexander Petushkov <aleks.petushkov@hotmail.com>
2016-02-22 02:13:32 +0600

    +tests

--- tmpl/main.cpp
+++ tmpl/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>     
 #include <algorithm>    
 #include <vector> 
+#include "gtest\gtest.h"
 
 using namespace std;
+
+class kkk;
+bool cmpa(const kkk & i, const  kkk & z);
+template <class RandomAccessIterator, class compare> void sortk(const RandomAccessIterator & first, const RandomAccessIterator & last, compare cmp);
 
 class kkk
 {
@@ -20,6 +25,39 @@ public:
 
 };
 
+
+/*
+Тестируем компаратор объектами класса ккк. Создается 2 объекта со значением 1 и 2.
+При подаче компаратора (1,2) -ожидаем истину
+При подаче компаратора (2,1) -ожидаем лож
+*/
+
+TEST(cmpr, kkk)  
+{
+	kkk k1(1), k2(2);
+	ASSERT_TRUE(cmpa(k1, k2) ); 
+	ASSERT_FALSE(cmpa(k2, k1)); 
+}
+
+/*
+Тестируется функция сортировки. Создается вектор, заполняется значениями, в сорт подается начальный и конечный итераторы, в результате ожидается отсортированный вектор.
+*/
+
+TEST(sotr)
+{
+	vector<int> vec;
+	vec.push_back(30);
+	vec.push_back(90);
+	vec.push_back(1);
+	vec.push_back(35);
+	sortk(vec.begin(), vec.end(), cmpa);
+	ASSERT_TRUE(vec[0] == 1);
+	ASSERT_TRUE(vec[1] == 30);
+	ASSERT_TRUE(vec[2] == 35);
+	ASSERT_TRUE(vec[3] == 90);
+}
+
+
 bool cmpa(const kkk & i, const  kkk & z)
 {
 	return (i<z);
@@ -27,7 +65,7 @@ bool cmpa(const kkk & i, const  kkk & z)
 
 
 /*
-template <class RandomAccessIterator> void sortk(RandomAccessIterator first, RandomAccessIterator last)
+template <class RandomAccessIterator> void sort(RandomAccessIterator first, RandomAccessIterator last)
 {
 	auto next = first;
 	bool out = true;
@@ -51,7 +89,7 @@ template <class RandomAccessIterator> void sortk(RandomAccessIterator first, Ran
 //*/
 
 
-template <class RandomAccessIterator, class compare> void sortk(RandomAccessIterator first, RandomAccessIterator last, compare cmp)
+template <class RandomAccessIterator, class compare> void sortk(const RandomAccessIterator & first, const RandomAccessIterator & last, compare cmp)
 {
 	auto next = first;
 	bool out = true;
@@ -75,7 +113,7 @@ template <class RandomAccessIterator, class compare> void sortk(RandomAccessIter
 
 
 
-int main()
+int main(int argc, char ** argv)
 {
 	kkk z = 10;
 	vector<kkk>_vect;
@@ -87,20 +125,20 @@ int main()
 	z = 7;
 	_vect.push_back(z);
 
-	//*
 	for (auto&elem : _vect)
 	{
 		cout << elem.z << endl;
 	}
-	//*/
 	cout << endl;
-
 	sortk(_vect.begin(), _vect.end(), cmpa);
-	//*
 	for (auto&elem : _vect)
 	{
 		cout << elem.z << endl;
 	}
-	//*/
+	cin.get();
+
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+
 	cin.get();
 }
